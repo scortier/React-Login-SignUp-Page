8c449f86e751d1448769cf0c2c8ea5b33dc138ba
scortier <onlytoaditya@gmail.com>
2021-03-07 22:26:09 +0530

    final update

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -18,7 +18,7 @@ class HomePage extends React.Component {
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h1>Hi {user.firstName}!</h1>
-                <p>You're logged in with React!!</p>
+                <p>Welcome!!</p>
                 <h3>All registered users:</h3>
                 {users.loading && <em>Loading users...</em>}
                 {users.error && <span className="text-danger">ERROR: {users.error}</span>}
